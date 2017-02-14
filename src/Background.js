fa7f1d3134152542609e96a2057d9a090477879d
Naeim Zarei <naeimzarei@dhcp02842.resnet-1x-wireless.unc.edu>
2017-02-13 21:43:23 -0500

    time

---
+++ src/Background.js
@@ -0,0 +1,10 @@
+/**
+ * Created by naeimzarei on 2/13/17.
+ */
+import React, {Component} from "react";
+
+export default class Background extends Component {
+    render() {
+        return null;
+    }
+}
